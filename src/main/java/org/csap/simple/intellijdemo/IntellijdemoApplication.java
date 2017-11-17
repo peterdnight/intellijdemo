8926aff054eb115d707c37e3ee935eec115734f9
peter <p.eternightingale@gmail.com>
2017-11-17 12:14:51 -0500

    test local cit

---
+++ src/main/java/org/csap/simple/intellijdemo/IntellijdemoApplication.java
@@ -0,0 +1,12 @@
+package org.csap.simple.intellijdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijdemoApplication.class, args);
+	}
+}
