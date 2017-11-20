7e5c2604161104621608f839dbf5d4c52289fb12
peter <p.eternightingale@gmail.com>
2017-11-20 09:26:47 -0500

    test local cit

---
+++ src/main/java/org/csap/simple/intellijdemo/Hello.java
@@ -0,0 +1,16 @@
+package org.csap.simple.intellijdemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hi")
+    public String hi() {
+
+        return "hi there now" ;
+    }
+
+}
